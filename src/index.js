fb2f096d28952061d758d8c45e95585580c6078d
Ralf Mueller <ralf@scardiecat.org>
2016-04-20 08:52:21 -0400

    tie in websocket (naive way) to be refactored later

--- src/index.js
+++ src/index.js
@@ -4,8 +4,29 @@ import { Provider } from 'react-redux';
 import configureStore from './stores';
 import App from './containers/App';
 
+
 const store = configureStore();
 
+let config = require('config');
+
+console.log(JSON.stringify(config.default));
+let wsConnectString = config.default.clusterManager.protocol
+ + '://'+config.default.clusterManager.host
+ + ':'+config.default.clusterManager.port
+ + config.default.clusterManager.path;
+
+var ws = new WebSocket(wsConnectString);
+ws.onmessage = function(event) {
+  console.log('data');
+  console.log(event);
+}
+
+
+
+//const socket = io({path:'/feed',transports: ['websocket']})
+// socket.on('news', news =>
+//   console.log(JSON.stringify(news))
+// );
 render(
   <Provider store={store}>
     <App />
